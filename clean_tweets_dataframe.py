93eebd8870b224283c8c224a30ef115efc6caf3a
Nebiyou <nebHailemariam@gmail.com>
2021-06-21 21:53:03 +0300

    Importing packages

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,3 +1,7 @@
+import json
+import pandas as pd
+from textblob import TextBlob
+    
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
@@ -20,17 +24,14 @@ class Clean_Tweets:
         """
         drop duplicate rows
         """
-        
-        ---
+        df.drop_duplicates()
         
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        ----
-        
-        ----
+        df['Date'] = pd.to_datetime(self.df['created_at'], errors='')
         
         df = df[df['created_at'] >= '2020-12-31' ]
         
@@ -41,10 +42,7 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.----
-        
-        ----
-        ----
+        df['polarity', 'subjectivity', 'retweet_count', 'favorite_count'] = pd.to_numeric(df['polarity', 'subjectivity','retweet_count', 'favorite_count'])
         
         return df
     
@@ -53,6 +51,6 @@ class Clean_Tweets:
         remove non english tweets from lang
         """
         
-        df = ----
+        df = df[df["lang"] == "en"]
         
         return df
--- extract_dataframe.py
+++ extract_dataframe.py
@@ -35,18 +35,24 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self)->list:
-        statuses_count 
+        statuses_count = None
         
     def find_full_text(self)->list:
-        text = 
+        text = []
+        for tweet in self.tweet_list:
+            text.append(tweet["full_text"])
+    
+        return text
        
     
     def find_sentiments(self, text)->list:
         
-        return polarity, self.subjectivity
+        return self.polarity, self.subjectivity
 
     def find_created_time(self)->list:
-       
+        created_at = []
+        for date in self.tweet_list:
+            created_at.append(date["created_at"])
         return created_at
 
     def find_source(self)->list:
@@ -129,7 +135,7 @@ if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
     columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("../covid19.json")
+    _, tweet_list = read_json("./data/covid19.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df() 
 
